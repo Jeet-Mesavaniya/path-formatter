745adb701bceac21d2bf47242fb35d11451afa41
Jeet Mesavaniya <jemishmesavaniya555@gmail.com>
2023-12-23 14:48:19 +0530

    ui and module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faTrash,faFileClipboard } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PATHFORMATTER';
+  varipstring: string="";
+  varopstring: string="";
+  CopyClipboardIcon = faFileClipboard;
+  TrashIcon = faTrash;
+  someLogic(){
+    var ipstring =  (document.getElementById("inputtxtbox") as HTMLInputElement).value;
+    this.varopstring = ipstring.replaceAll('//',"/");
+  }
+
+  copyTextIP() {
+    const text = (document.getElementById("inputtxtbox") as HTMLInputElement).value;
+    navigator.clipboard.writeText(text);
+  }
+  copyTextOP() {
+    const text = (document.getElementById("outputtxtbox") as HTMLInputElement).value;
+    navigator.clipboard.writeText(text);
+  }
+
+  clearTextALL() {
+    (document.getElementById("inputtxtbox") as HTMLInputElement).value = '';
+    (document.getElementById("outputtxtbox") as HTMLInputElement).value = '';
+  }
 }
